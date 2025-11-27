ae918296737706de1f1a5685d10c6d7ca509a019
lukmanazril868-debug <lukmanazril868@gmail.com>
2025-11-27 10:38:18 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
-const buttons = document.querySelectorAll(".nav-btn");
-const pages = document.querySelectorAll(".page");
-
-buttons.forEach(btn => {
-  btn.addEventListener("click", () => {
-
-    // aktifkan tombol
-    buttons.forEach(b => b.classList.remove("active"));
-    btn.classList.add("active");
-
-    // tampilkan page
-    const page = btn.dataset.page;
-    pages.forEach(p => p.classList.remove("active"));
-    document.getElementById(page).classList.add("active");
-  });
-});
+function showPage(id) {
+  document.querySelectorAll("section").forEach(sec => sec.classList.remove("active"));
+  document.getElementById(id).classList.add("active");
+}
