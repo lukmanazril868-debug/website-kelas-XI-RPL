1cedd2f7bf47ffd5c93aa73520fae88ef6388b3b
lukmanazril868-debug <lukmanazril868@gmail.com>
2025-11-27 09:36:01 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-function showPage(id) {
-  document.querySelectorAll("section").forEach(sec => sec.classList.remove("active"));
-  document.getElementById(id).classList.add("active");
-}
+const buttons = document.querySelectorAll('.nav-btn');
+const pages = document.querySelectorAll('.page');
+
+buttons.forEach(btn => {
+  btn.addEventListener('click', () => {
+    // clear active button
+    buttons.forEach(b => b.classList.remove("active"));
+    btn.classList.add("active");
+
+    // show page
+    const pageName = btn.dataset.page;
+    pages.forEach(p => {
+      p.classList.remove("active");
+      if (p.id === pageName) p.classList.add("active");
+    });
+  });
+});
